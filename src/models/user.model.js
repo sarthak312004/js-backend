1a928caaaddc6d5b24107a5d874a126194e5ccb8
srthk31 <sarthak31122004m@gmail.com>
2026-09-14 20:20:30 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,49 @@
-import mongoose from 'mongoose'
+import mongoose,{Schema, model, trusted} from 'mongoose'
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
     {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index: true
+        },
+        avatar:{
+            type:String,
+            required: true,
+        },
+        coverImg:{
+            type:String,
+        },
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refershToken:{
+            type:String
+        }
 
     },{timestamps:true}
 )
-export const User = mongoose.model('User', userSchema)
+export const User = model('User', userSchema)
