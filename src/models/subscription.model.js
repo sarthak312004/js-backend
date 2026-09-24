27a10dcca879ad72ca151f5201218c4a52db8a7e
srthk31 <sarthak31122004m@gmail.com>
2026-09-24 19:45:04 +0530

    Add user account management features: change password, update details, and avatar handling

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,6 +2,16 @@ import {Schema ,model} from 'mongoose'
 
 const subscriptionSchema = new Schema (
     {
+        subscriber:{
+            type:Schema.Types.ObjectId, //one who is subscribing
+            ref:"User"
+        },
+
+        channel:{
+            type:Schema.Types.ObjectId, //one to whom 'subscriber' is subscribing
+            ref:"User"
+        },
+
 
     },{timestamps:true}
 )
